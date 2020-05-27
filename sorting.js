7429e5ba3a83340d35bda8ebc245e157288feb06
Kangbeom Kim <kkb931115@gmail.com>
2020-05-27 21:47:20 +0900

    버블 정렬 구현

---
+++ sorting.js
@@ -0,0 +1,24 @@
+/*  Writer: EvenNewbieImthePro 
+    Date: 25, May, 2020
+    Content: Implement sorting element by sorting algorithms. 
+*/
+
+const input = [6,1,3,2,4];
+
+function bubbleSort(array) {
+    var arrayNeedSort = array;
+    console.log(`초기 배열: ${arrayNeedSort}`);
+
+    for(var i=0; i<arrayNeedSort.length; i++) {
+        for(var j=i; j<arrayNeedSort.length-1; j++) {
+
+            if( arrayNeedSort[j] > arrayNeedSort[j+1]) {
+                var temp = arrayNeedSort[j];
+                arrayNeedSort[j] = arrayNeedSort[j+1];
+                arrayNeedSort[j+1] = temp;
+            }
+        }
+    }
+    console.log(`정렬 후: ${arrayNeedSort}`)
+}
+bubbleSort(input)
